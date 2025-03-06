9109ae0ee51dbfcc2db7a3037ca1734138b21060
Jignesh Raiyani <jignesh.raiyani1@gmail.com>
2025-03-06 09:06:58 +0530

    Added environment config file

--- blog_circle/lib/core/secrets/app_secrets.dart
+++ blog_circle/lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
   static const supabaseUrl = 'https://dpongfzsprxpscvxaufl.supabase.co';
   static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwb25nZnpzcHJ4cHNjdnhhdWZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNzY4NDMsImV4cCI6MjA1NDY1Mjg0M30.ycJ9yE3Wdr-nkDOXP1AIL-tFe32V8Ug_yny1Gs8O5Fs';
+      '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwb25nZnpzcHJ4cHNjdnhhdWZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNzY4NDMsImV4cCI6MjA1NDY1Mjg0M30.ycJ9yE3Wdr-nkDOXP1AIL-tFe32V8Ug_yny1Gs8O5Fs';
 }
